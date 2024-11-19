8878911ebbf7b884c7082280151a2fbe64f67706
shishirsindhe <shishirsindhe99@gmail.com>
2024-11-19 10:51:35 +0530

    task2

--- GH_Actions.py
+++ GH_Actions.py
@@ -1,1 +1,21 @@
 print("Something")
+
+
+
+class Employee:
+    def __init__(self,name,age,salary):
+        self.name=name
+        self.age=age
+        self.salary=salary
+
+    def Total_CTC(self):
+        total_ctc=0
+        total_ctc+=self.salary*12
+        return total_ctc
+
+e1=Employee("Bill",26,1000)
+e2=Employee("steve",28,4000)
+print(e1.salary)
+print(e1.Total_CTC())
+print(e2.salary)
+print(e2.Total_CTC())
